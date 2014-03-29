45d6e5b853de11f4ad2fdd49fd22760143037428
albart <albart@sbcglobal.net>
2014-03-29 00:25:22 -0500

    Update register_client.php

--- public/register_client.php
+++ public/register_client.php
@@ -6,13 +6,26 @@
     // if form was submitted
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        // TODO
-        if (empty($_POST["username"]))
+        if (empty($_POST["namelast"]))
+            apologize("You must provide a last name.")
+        else if (empty($_POST["namefirst"]))
+            apologize("You must provide a first name.")
+        else if (empty($_POST["namemi"]))
+            apologize("You must provide a middle initial.")
+        else if (empty($_POST["zipcode"]))
+            apologize("You must provide a zip code.")
+        else if (empty($_POST["username"]))
             apologize("You must provide a username.");
+        else if (empty($_POST["email"]))
+            apologize("You must provide an email address.");
         else if (empty($_POST["password"]))
             apologize("You must provide a password.");
         else if ($_POST["password"] !== $_POST["confirmation"])
             apologize("Passwords do not match!");
+        else if (empty($_POST["reporthist"]))
+            apologize("You must select whether to receive a report history.");
+        else if (empty($_POST["datelist"]))
+            apologize("You must select a date to receive reports.");
         else
         {
             $return = query("INSERT INTO users (username, hash, cash) VALUES(?, ?, 10000.00)", $_POST["username"], crypt($_POST["password"]));
