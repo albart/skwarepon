9a92404e1de9952fc29bf5d9d9815842ec5daf20
albart <albart@sbcglobal.net>
2014-03-29 01:34:15 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -19,6 +19,24 @@
 
     // enable sessions
     session_start();
+    
+    query("create database if not exists " . DATABASE);
+    query("use " . DATABASE);
+    query("create table if not exists clients (
+        id int(10) auto_increment,
+        namelast varchar(40),
+        namefirst varchar(40),
+        namemi char(1),
+        zipcode int, 
+        username varchar(255),
+        email varchar(255),
+        password varchar(255),
+        reporthist char(1),
+        datelist tinyint(2),
+        primary key (id),
+        unique key (username),
+        unique key (email)
+        ) engine=InnoDB");
 
     // require authentication for most pages
     if (!preg_match("{(?:login|logout|register_customer|register_client)\.php$}", $_SERVER["PHP_SELF"]))
