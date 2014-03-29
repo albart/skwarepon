f9e445ebd4751cb1d2db297a2e8d877cef52574f
albart <albart@sbcglobal.net>
2014-03-28 22:58:02 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -21,7 +21,7 @@
     session_start();
 
     // require authentication for most pages
-    if (!preg_match("{(?:login|logout|register|register_client)\.php$}", $_SERVER["PHP_SELF"]))
+    if (!preg_match("{(?:login|logout|register_customer|register_client)\.php$}", $_SERVER["PHP_SELF"]))
     {
         if (empty($_SESSION["id"]))
         {
