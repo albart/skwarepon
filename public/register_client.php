4e47960b92176799150ccbcefb57e4474b6b31b8
albart <albart@sbcglobal.net>
2014-03-31 20:33:11 -0500

    Update register_client.php

--- public/register_client.php
+++ public/register_client.php
@@ -28,7 +28,7 @@
             apologize("You must provide a username.");
         else if (empty($_POST["email"]))
             apologize("You must provide an email address.");
-        else if ((filter_var($_POST["email"])) === FALSE)
+        else if ((filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) === FALSE)
             apologize("Your email is invalid.")
         else if (empty($_POST["password"]))
             apologize("You must provide a password.");
