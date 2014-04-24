82d67b302578cfefc481678844e7f9b1e7609d00
2itchy2scratchy <2itchy2scratchy@users.noreply.github.com>
2014-04-24 15:50:03 -0500

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
             </div>
 
             <div id="bottom">
-                Copyright &#169; SkwarePon
+                Copyright &#169; SkwarePon   -- Contact us via email: Mr.anonymo@att.net -- via phone: 555-111-2222
             </div>
 
         </div>
