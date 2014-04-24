af332d11d99afdb1010e4d7ea11a6a85a13cfea8
2itchy2scratchy <2itchy2scratchy@users.noreply.github.com>
2014-04-24 15:20:03 -0500

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -9,9 +9,9 @@
         <link href="/css/styles.css" rel="stylesheet"/>
 
         <?php if (isset($title)): ?>
-            <title>Skwarepon: <?= htmlspecialchars($title) ?></title>
+            <title>SkwarePon: <?= htmlspecialchars($title) ?></title>
         <?php else: ?>
-            <title>Skwarepon</title>
+            <title>SkwarePon</title>
         <?php endif ?>
 
         <script src="/js/jquery-1.10.2.min.js"></script>
@@ -25,7 +25,7 @@
         <div class="container">
 
             <div id="top">
-                <a href="/"><img alt="Skwarepon" src="<!--TBD -->"/></a>
+                <a href="/"><img alt="SkwarePon" src="<!--TBD -->"/></a>
             </div>
 
             <div id="middle">
