1831f7b110127cb316ec59aa02e53bfb0d2a57f3
John Harvard <albart-at-sbcglobal.net>
2014-04-30 20:33:23 -0400

    thirteenth

---
+++ public/emailtest.php
@@ -0,0 +1,8 @@
+<?php
+
+    // configuration
+    require("../includes/config.php");
+
+    emailsp("albart@sbcglobal.net", "Skwarepon registration", "Registration was successful with Skwarepon.");
+
+?>
