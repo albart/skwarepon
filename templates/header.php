b72dc47148d89853d910bf805d267da7a1eca1e3
walrus35 <patrickalloyd@gmail.com>
2014-05-06 19:28:44 -0400

    changes to css file and header

--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 
+        
     <head>
-
-        <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+   	
+		<link href="/css/skwarepon.css" media="screen" rel="stylesheet" type="text/css">
+<!--		<link href="/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="/css/bootstrap-theme.min.css" rel="stylesheet"/>
-        <link href="/css/styles.css" rel="stylesheet"/>
-
+        <link href="/css/styles.css" rel="stylesheet"/>  
+-->
+  
         <?php if (isset($title)): ?>
             <title>SkwarePon: <?= htmlspecialchars($title) ?></title>
         <?php else: ?>
@@ -22,10 +25,9 @@
 
     <body>
 
-        <div class="container">
+        <div class="main-container">
 
-            <div id="top">
-                <a href="/"><img alt="SkwarePon" src="<!--TBD -->"/></a>
-            </div>
+            <div id="main">
+                <h1><a href="/"><img alt="SkwarePon" src="images/ponwhite.png"/></a></h1>
+          
 
-            <div id="middle">
