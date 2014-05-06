8cc050e25235becbb8bc28652aed1b200cc251f6
John Harvard <albart-at-sbcglobal.net>
2014-05-06 16:41:47 -0400

    fifteenth

--- includes/config.php
+++ includes/config.php
@@ -20,25 +20,6 @@
     // enable sessions
     session_start();
     
-    /*query("create database if not exists " . DATABASE);
-    query("use " . DATABASE);
-    query("create table if not exists clients (
-        id int(10) auto_increment,
-        namelast varchar(40),
-        namefirst varchar(40),
-        namemi char(1),
-        phone int,
-        zipcode varchar(255), 
-        username varchar(255),
-        email varchar(255),
-        password varchar(255),
-        reporthist char(1),
-        datelist tinyint(2),
-        primary key (id),
-        unique key (username),
-        unique key (email)
-        ) engine=InnoDB");*/
-
     // require authentication for most pages
     if (!preg_match("{(?:login|logout|register_customer|register_client)\.php$}", $_SERVER["PHP_SELF"]))
     {
