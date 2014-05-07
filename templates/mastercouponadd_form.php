b177292eb65bf673a53db7c5488baa49fdda77f4
John Harvard <albart-at-sbcglobal.net>
2014-05-06 22:31:07 -0400

    seventeenth

--- templates/mastercouponadd_form.php
+++ templates/mastercouponadd_form.php
@@ -4,8 +4,8 @@
             <input autofocus class="form-control" name="eventID" placeholder="event ID" type="text"/>
             <input class="form-control" name="shortdesc" placeholder="Short Description" type="text"/>
             <input class="form-control" name="longdesc" placeholder="Long Description" type="text"/>
-            <input class="form-control" name="validfrom" placeholder="Valid from Date format yyyy-mm-dd" type="text"/>
-            <input class="form-control" name="validthru" placeholder="Valid thru Date format yyyy-mm-dd" type="text"/>
+            <input class="form-control" name="validfrom" placeholder="Valid from Date format yyyy-mm-dd" type="date"/>
+            <input class="form-control" name="validthru" placeholder="Valid thru Date format yyyy-mm-dd" type="date"/>
             <input class="form-control" name="limit" placeholder="limit" type="text"/>
             <input class="form-control" name="thresholdtext" placeholder="Text Threshold" type="text"/>
             <input class="form-control" name="thresholdemail" placeholder="Email Threshold" type="text"/>
