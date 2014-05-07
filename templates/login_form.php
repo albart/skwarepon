5ff6d48e2d99ac6905810bbc97131ac8f6447b52
walrus35 <patrickalloyd@gmail.com>
2014-05-06 22:32:48 -0400

    changes to the login_form

--- templates/login_form.php
+++ templates/login_form.php
@@ -1,14 +1,38 @@
-<form action="login.php" method="post">
-    <label>UserName :</label>
-    <input autofocus class="form-control" type="text" name="username"/><br />
-    <label>Password :</label>
-    <input type="password" name="password"/><br/>
-    <input type="submit" name="type" value="Login as"/><br/>
-    <input checked class="form-control" name="type" type="radio" value="customer"/>Customer
-    <input class="form-control" name="type" type="radio" value="client"/>Client
-</form>
+<div id="form-oldschool">
 
-<div>
-    <a href = "register_customer.php">Register customer</a>
-    <a href = "register_client.php">Register client</a>
+    <form action="login.php" method="post">
+    
+        <fieldset>
+            <label for="login">UserName :</label>
+            <input autofocus class="form-control" type="text" name="username"/><br/>
+        </fieldset>
+        <fieldset>
+            <label for="login">Password :</label>
+            <input type="password" name="password"/><br/>
+        </fieldset>
+        <input class="button" type="submit" name="type" value="Sign In" tabindex="3">
+        <label for="login">
+            <input checked class="form-control" name="type" type="radio" value="customer"/>Customer
+            <input class="form-control" name="type" type="radio" value="client"/>Client
+        </label>
+    </form>
+</div>
+        
+
+<div id="old-school" style="text-align:right" ! important>
+    <form action="register_customer.php" method="get">
+        
+        <fieldset>
+            <label for="login">
+                </br> </br> Not a Member? </br> Sign up: </br>
+            </label>
+        </fieldset>
+        <fieldset>
+            <input class="button" type="submit" name="type" value="Customer" tabindex="3">
+        </fieldset>
+    </form>
+    <form action="register_client.php" method="get">
+        <fieldset>
+            <input class="button" type="submit" name="type" value="Client" tabindex="3">
+        </fieldset>
 </div>
